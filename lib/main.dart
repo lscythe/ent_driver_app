130902261c233397b58065fabadcdd4250014299
lscythe <lscythe.git@proton.me>
2023-11-17 04:06:08 +0700

    add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:driver/app/driver_app.dart';
 import 'package:driver/firebase_options.dart';
 import 'package:driver/locator/locator.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -11,6 +13,16 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+
   await configureDependencies();
+
   runApp(const DriverApp());
 }
